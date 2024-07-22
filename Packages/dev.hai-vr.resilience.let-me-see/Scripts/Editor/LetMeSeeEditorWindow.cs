c32272268d723aec4f63aa147cbf21c4c4fd85b7
Haï~ <hai-vr@users.noreply.github.com>
2024-07-23 00:45:53 +0200

    Extract all strings for localization

--- Packages/dev.hai-vr.resilience.let-me-see/Scripts/Editor/LetMeSeeEditorWindow.cs
+++ Packages/dev.hai-vr.resilience.let-me-see/Scripts/Editor/LetMeSeeEditorWindow.cs
@@ -11,6 +11,31 @@ namespace Resilience.LetMeSee
 {
     public class LetMeSeeEditorWindow : EditorWindow
     {
+        private const string AdvancedLabel = "Advanced";
+        private const string AlignmentLabel = "Alignment";
+        private const string CameraModeLabel = "Camera Mode";
+        private const string CursorColorLabel = "Cursor color";
+        private const string DoNotSwitchSceneTabsLabel = "Do not switch Scene tabs";
+        private const string EditYourHeightLabel = "Edit your height";
+        private const string ForceUseOpenXRLabel = "Force use OpenXR";
+        private const string FreezeCameraLabel = "Freeze Camera";
+        private const string InitializeXROnStartupLabel = "(DEBUG) Initialize XR on Startup";
+        private const string InstallOpenXRLabel = "Install OpenXR";
+        private const string IpdAndScaleLabel = "IPD and Scale";
+        private const string LastValidPoseDataLabel = "Last valid pose data: {0:0.00000}, {1:0.00000}, {2:0.00000}";
+        private const string MoveCameraUpLabel = "Move camera up";
+        private const string MsgOpenXRMissing = "OpenXR is missing from the project.\nInstall OpenXR?";
+        private const string PleaseWaitLabel = "Please wait...";
+        private const string RecenterViewLabel = "Recenter view";
+        private const string RescaleLabel = "Rescale";
+        private const string RescaledHeightFeetLabel = "Rescaled height (ft)";
+        private const string RescaledHeightMetresLabel = "Rescaled height (m)";
+        private const string ResetScaleLabel = "Reset scale";
+        private const string RunLabel = "Run";
+        private const string ShowCursorLabel = "Show cursor";
+        private const string YourHeightFeetLabel = "Your height (ft)";
+        private const string YourHeightMetresLabel = "Your height (m)";
+        
         private Vector2 _scrollPos;
         private bool _waitOpenXR;
 
@@ -33,26 +58,26 @@ namespace Resilience.LetMeSee
             _scrollPos = GUILayout.BeginScrollView(_scrollPos, GUILayout.Height(position.height - EditorGUIUtility.singleLineHeight));
             
             var isEnabled = LetMeSeeCore.Instance.Enabled;
-            if (ColoredBackground(isEnabled, Color.red, () => GUILayout.Button("Run")))
+            if (ColoredBackground(isEnabled, Color.red, () => GUILayout.Button(RunLabel)))
             {
                 if (isEnabled) LetMeSeeCore.Instance.DoHardStop();
                 else LetMeSeeCore.Instance.DoHardStart();
             }
-            if (ColoredBackground(LetMeSeeCore.Instance.FreezeCamera, new Color(0.77f, 1f, 0f, 0.75f), () => GUILayout.Button("Freeze Camera")))
+            if (ColoredBackground(LetMeSeeCore.Instance.FreezeCamera, new Color(0.77f, 1f, 0f, 0.75f), () => GUILayout.Button(FreezeCameraLabel)))
             {
                 LetMeSeeCore.Instance.FreezeCamera = !LetMeSeeCore.Instance.FreezeCamera;
             }
             EditorGUILayout.Separator();
             
-            if (GUILayout.Button("Recenter view", GUILayout.Height(50)))
+            if (GUILayout.Button(RecenterViewLabel, GUILayout.Height(50)))
             {
                 LetMeSeeCore.Instance.DoRecenter();
             }
             EditorGUILayout.Separator();
             
-            EditorGUILayout.LabelField("IPD and Scale", EditorStyles.boldLabel);
+            EditorGUILayout.LabelField(IpdAndScaleLabel, EditorStyles.boldLabel);
             var isHeightEditable = LetMeSeeUserSettings.IsHeightEditable;
-            var newHeightEditable = EditorGUILayout.Toggle("Edit your height", isHeightEditable);
+            var newHeightEditable = EditorGUILayout.Toggle(EditYourHeightLabel, isHeightEditable);
             if (newHeightEditable != isHeightEditable)
             {
                 LetMeSeeUserSettings.IsHeightEditable = newHeightEditable;
@@ -62,14 +87,14 @@ namespace Resilience.LetMeSee
             {
                 EditorGUI.BeginDisabledGroup(!LetMeSeeUserSettings.IsHeightEditable);
                 var heightInMeters = LetMeSeeUserSettings.UserHeight;
-                var newHeight = EditorGUILayout.FloatField("Your height (m)", heightInMeters);
+                var newHeight = EditorGUILayout.FloatField(YourHeightMetresLabel, heightInMeters);
                 if (newHeight != heightInMeters)
                 {
                     LetMeSeeUserSettings.UserHeight = newHeight;
                 }
 
                 var heightInFeet = LetMeSeeUserSettings.UserHeight / 0.3048f;
-                var newHeightInFeet = EditorGUILayout.FloatField("Your height (ft)", heightInFeet);
+                var newHeightInFeet = EditorGUILayout.FloatField(YourHeightFeetLabel, heightInFeet);
                 if (newHeightInFeet != heightInFeet)
                 {
                     LetMeSeeUserSettings.UserHeight = newHeightInFeet * 0.3048f;
@@ -81,14 +106,14 @@ namespace Resilience.LetMeSee
                 var heightInMeters = LetMeSeeUserSettings.UserHeight;
                 var heightInFeet = LetMeSeeUserSettings.UserHeight / 0.3048f;
                 var rescale0 = LetMeSeeUserSettings.Rescale;
-                var rescaledHeight = EditorGUILayout.FloatField("Rescaled height (m)", heightInMeters * rescale0);
+                var rescaledHeight = EditorGUILayout.FloatField(RescaledHeightMetresLabel, heightInMeters * rescale0);
                 if (rescaledHeight != heightInMeters * rescale0 && rescaledHeight >= 0)
                 {
                     LetMeSeeUserSettings.Rescale = rescaledHeight / heightInMeters;
                 }
 
                 var rescale1 = LetMeSeeUserSettings.Rescale;
-                var rescaledHeightInFeet = EditorGUILayout.FloatField("Rescaled height (ft)", heightInFeet * rescale1);
+                var rescaledHeightInFeet = EditorGUILayout.FloatField(RescaledHeightFeetLabel, heightInFeet * rescale1);
                 if (rescaledHeightInFeet != heightInFeet * rescale1 && rescaledHeightInFeet >= 0)
                 {
                     LetMeSeeUserSettings.Rescale = rescaledHeightInFeet / heightInFeet;
@@ -102,30 +127,30 @@ namespace Resilience.LetMeSee
             else
             {
                 var rescale2 = LetMeSeeUserSettings.Rescale;
-                var newRescale = EditorGUILayout.FloatField("Rescale", rescale2);
+                var newRescale = EditorGUILayout.FloatField(RescaleLabel, rescale2);
                 if (newRescale != rescale2 && newRescale >= 0)
                 {
                     LetMeSeeUserSettings.Rescale = rescale2;
                 }
             }
-            if (GUILayout.Button("Reset scale"))
+            if (GUILayout.Button(ResetScaleLabel))
             {
                 LetMeSeeUserSettings.Rescale = 1;
             }
             
             EditorGUILayout.Separator();
-            EditorGUILayout.LabelField("Alignment", EditorStyles.boldLabel);
-            var newCameraMode = (LetMeSeeCameraMode)EditorGUILayout.EnumPopup(new GUIContent("Camera Mode"), LetMeSeeUserSettings.CameraMode);
+            EditorGUILayout.LabelField(AlignmentLabel, EditorStyles.boldLabel);
+            var newCameraMode = (LetMeSeeCameraMode)EditorGUILayout.EnumPopup(new GUIContent(CameraModeLabel), LetMeSeeUserSettings.CameraMode);
             if (newCameraMode != LetMeSeeUserSettings.CameraMode)
             {
                 LetMeSeeUserSettings.CameraMode = newCameraMode;
             }
-            if (ColoredBackground(LetMeSeeCore.Instance.LockSceneView, new Color(0f, 1f, 1f, 0.75f), () => GUILayout.Button("Do not switch Scene tabs")))
+            if (ColoredBackground(LetMeSeeCore.Instance.LockSceneView, new Color(0f, 1f, 1f, 0.75f), () => GUILayout.Button(DoNotSwitchSceneTabsLabel)))
             {
                 LetMeSeeCore.Instance.LockSceneView = !LetMeSeeCore.Instance.LockSceneView;
             }
 
-            var showCursor = EditorGUILayout.Toggle("Show cursor", LetMeSeeUserSettings.ShowCursor);
+            var showCursor = EditorGUILayout.Toggle(ShowCursorLabel, LetMeSeeUserSettings.ShowCursor);
             if (showCursor != LetMeSeeUserSettings.ShowCursor)
             {
                 LetMeSeeUserSettings.ShowCursor = showCursor;
@@ -133,7 +158,7 @@ namespace Resilience.LetMeSee
             
             
             var cursorColor = LetMeSeeUserSettings.CursorColor;
-            var newColor = EditorGUILayout.ColorField(new GUIContent("Cursor color"), cursorColor);
+            var newColor = EditorGUILayout.ColorField(new GUIContent(CursorColorLabel), cursorColor);
             if (newColor != cursorColor)
             {
                 LetMeSeeUserSettings.CursorColor = newColor;
@@ -142,7 +167,7 @@ namespace Resilience.LetMeSee
 
             if (newCameraMode != LetMeSeeCameraMode.SceneView)
             {
-                var newMoveUp = EditorGUILayout.Slider("Move camera up", LetMeSeeUserSettings.MoveUp, 0, 1);
+                var newMoveUp = EditorGUILayout.Slider(MoveCameraUpLabel, LetMeSeeUserSettings.MoveUp, 0, 1);
                 if (newMoveUp != LetMeSeeUserSettings.MoveUp)
                 {
                     LetMeSeeUserSettings.MoveUp = newMoveUp;
@@ -150,8 +175,8 @@ namespace Resilience.LetMeSee
             }
             
             EditorGUILayout.Separator();
-            EditorGUILayout.LabelField("Advanced", EditorStyles.boldLabel);
-            var useOpenXR = EditorGUILayout.Toggle("Force use OpenXR", LetMeSeeUserSettings.ForceUseOpenXR);
+            EditorGUILayout.LabelField(AdvancedLabel, EditorStyles.boldLabel);
+            var useOpenXR = EditorGUILayout.Toggle(ForceUseOpenXRLabel, LetMeSeeUserSettings.ForceUseOpenXR);
             if (useOpenXR != LetMeSeeUserSettings.ForceUseOpenXR)
             {
                 LetMeSeeUserSettings.ForceUseOpenXR = useOpenXR;
@@ -161,9 +186,9 @@ namespace Resilience.LetMeSee
             if (LetMeSeeUserSettings.ForceUseOpenXR)
             {
                 EditorGUILayout.BeginHorizontal();
-                EditorGUILayout.HelpBox("OpenXR is missing from the project.\nInstall OpenXR?", MessageType.Warning);
+                EditorGUILayout.HelpBox(MsgOpenXRMissing, MessageType.Warning);
                 EditorGUI.BeginDisabledGroup(_waitOpenXR);
-                if (GUILayout.Button(_waitOpenXR ? "Please wait..." : "Install OpenXR", GUILayout.Width(200), GUILayout.Height(EditorGUIUtility.singleLineHeight * 2)))
+                if (GUILayout.Button(_waitOpenXR ? PleaseWaitLabel : InstallOpenXRLabel, GUILayout.Width(200), GUILayout.Height(EditorGUIUtility.singleLineHeight * 2)))
                 {
                     Client.Add("com.unity.xr.openxr");
                     _waitOpenXR = true;
@@ -175,7 +200,7 @@ namespace Resilience.LetMeSee
 
             if (XRGeneralSettings.Instance != null)
             {
-                var xrOnStartup = EditorGUILayout.Toggle("(DEBUG) Initialize XR on Startup", XRGeneralSettings.Instance.InitManagerOnStart);
+                var xrOnStartup = EditorGUILayout.Toggle(InitializeXROnStartupLabel, XRGeneralSettings.Instance.InitManagerOnStart);
                 if (xrOnStartup != XRGeneralSettings.Instance.InitManagerOnStart)
                 {
                     XRGeneralSettings.Instance.InitManagerOnStart = xrOnStartup;
@@ -183,7 +208,7 @@ namespace Resilience.LetMeSee
             }
 
             var lastValidPos = LetMeSeeCore.Instance.LastValidPoseDataPos();
-            EditorGUILayout.LabelField(string.Format(CultureInfo.InvariantCulture, "Last valid pose data: {0:0.00000}, {1:0.00000}, {2:0.00000}", lastValidPos.x, lastValidPos.y, lastValidPos.z));
+            EditorGUILayout.LabelField(string.Format(CultureInfo.InvariantCulture, LastValidPoseDataLabel, lastValidPos.x, lastValidPos.y, lastValidPos.z));
             
             EditorGUILayout.EndScrollView();
         }
